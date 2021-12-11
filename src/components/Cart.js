a8b7ebcfc6312207a227572c3e20ee313e1e685f
Hernan Amaiz <66435038+Maizo21@users.noreply.github.com>
2021-12-10 21:51:50 -0300

    Modificacion en cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useCartContext, CartProvider } from "./CartContext";
 
 const Cart = () => {
+  const { deleteFromCart } = useCartContext();
+  const { quantityItem } = useCartContext();
+  const { totalPrice } = useCartContext();
+  const { emptyCart } = useCartContext();
+  const { items } = useCartContext();
+
+  console.log(items);
+
+  /*   const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    setCartItems(items);
+  }, [items]);
+ */
   return (
     <>
       <div className="ms-4 mt-2">
         <h2>Carrito</h2>
 
-        <p>Productos...</p>
+        {items.length > 0 ? (
+          items && items != null ? (
+            <div>
+              {items.map((item, index) => (
+                <div key={index} className="flex justify-between">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="w-16 h-16 mr-2"
+                  />
+                  <h2>{item.name}</h2>
+                  <p>{quantityItem(item)}</p>
+                  <small>{deleteFromCart(item)}</small>
+                </div>
+              ))}
+              <p>Total: {totalPrice()}</p>
+            </div>
+          ) : null
+        ) : (
+          <p>No hay productos en el carrito</p>
+        )}
 
         <div className="d-flex gap-2">
+          <div
+            onClick={() => {
+              emptyCart();
+            }}
+            className="btn btn-danger"
+          >
+            Vaciar Carrito
+          </div>
           <Link to="/" className="btn btn-primary text-decoration-none">
             Seguir comprando
           </Link>
